a7d3ddfc3b5358e413d216aa8a15b71a46192b50
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-31 14:07:52 +0530

    Add files via upload

---
+++ GreaterOrLess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonUsingOOPs
+{
+   class GreaterOrLess
+    {
+        public int x1=1, x2=1;
+        public int y1=1, y2=1;
+
+        public void Compare()
+        {
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)
+                    * (y2 - y1));
+
+            Console.WriteLine("Distance between two points :" + distance);
+
+
+
+            string s1 = distance.ToString();
+            string s2 = distance.ToString();
+
+            Console.WriteLine(s1.Equals(s2));
+
+            Console.WriteLine(s1.CompareTo(s2));
+
+        }
+    }
+}
