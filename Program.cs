c1cea4a8185017667bf94bb651113843ca936f96
PratikChitruk <pratik.chitruk@gmail.com>
2022-03-31 14:04:32 +0530

    program.cs push for LineComaprisonOops

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonUsingOOPs 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison using OOPs");
+
+
+            CalculateLength calculateLength = new CalculateLength();
+
+            calculateLength.length();
+
+            CheckEquality checkEquality = new CheckEquality();
+
+            checkEquality.Equality();
+
+            GreaterOrLess greaterOrLess = new GreaterOrLess();
+
+            greaterOrLess.Compare();
+
+
+
+
+        }
+    }
+}
